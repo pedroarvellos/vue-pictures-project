4cfd9035ef840fdfa5a5dfd5cec96cfa6d76369e
Pedro Arvellos <pedroarvellos@hotmail.com>
2017-12-26 08:50:21 -0200

    First commit with the project structure.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1> {{ titulo }}</h1>
+
+    <ul>
+      <li v-for="foto of fotos">
+        <img v-bind:src="foto.url" v-bind:alt="foto.titulo">  <!-- Quando é atributo eu não posso fazer simplesmente a interpolação, como neste caso, que tenho os atributos da img. Neste caso, eu preciso fazer um v-bind (caso não queira escrever v-bind, posso apenas colocar : antes. -->
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      titulo: 'Alurapic',
+      fotos: [
+        {
+          url: "http://tudosobrecachorros.com.br/wp-content/uploads/remedios-proibidos-cachorro-766x526.jpg",
+          titulo: "cachorro1"
+      },
+        {
+        url: "http://tudosobrecachorros.com.br/wp-content/uploads/remedios-proibidos-cachorro-766x526.jpg",
+        titulo: "cachorro2"
+      }],
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue' //vue é um módulo no qual nós importamos o Global View Object Vue. 
+import App from './App.vue'
+
+import VueResource from 'vue-resource'; //Estou importando o meu vue-resource que baixei como dependência pelo prompt, a partir do comando npm install. Esta dependência será utilizada para eu consumir os dados da minha API.
+//O vue-resource é aquele que disponibiliza artefatos especializados na realização de requisições assíncronas ao mesmo tempo em que se integra com o ecossistema do Vue.
+Vue.user(VueResource); //Estou agora dizendo para o VUE utilizar do meu pacote. 
+
+new Vue({ //Esta é uma Vue instance, que foi criada a partir do Global View Object, para renderizar um componente em index.html
+  el: '#app',
+  render: h => h(App) //App está sendo importando do App.vue
+})
