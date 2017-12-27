75575f1f271c26969537e77a6f889c9bfe32c813
Pedro Arvellos <pedroarvellos@hotmail.com>
2017-12-27 13:59:26 -0200

    Adding new components!

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,59 @@
   <div class="corpo">
     <h1 class="centralizado">{{ titulo }}</h1>
     
+    <input type="search" class="filtro" v-on:input = "filtro = $event.target.value" placeholder="filtre pelo título"/> <!-- Toda vez que o evento este evento v-on é disparado, ele executa da seguinte forma: pega filtro que é uma variável declarada dentro do meu data, e ele vai receber $event (objeto do vue.js), .target significa que é quem disparou o evento, e o value é o valor inserido. Resumindo, quando este evento é disparado, é colocado dentro do filtro o que está sendo digitado dinamicamente. Toda vez que estou digitando ele vai rodando este evento.-->
+    {{ filtro }}
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos" :key="foto.url">
-
+      <li class="lista-fotos-item" v-for="foto of fotosComfiltro" :key="foto.url">
           <!-- Quando ele chega aqui, ele percebe que deve trocar esta notação pelo Painel que foi declarado (meu-painel, foi declarado em components como um "apelido" para Painel). Como ele vai trocar esta tag pelo tag Painel, tudo que tiver aqui dentro, ao menos que exita um "slot", será descartado. -->
           <meu-painel :titulo="foto.titulo">
-            <img class="imagem-responsiva" v-bind:src="foto.url" v-bind:alt="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo">
+            
+          </imagem-responsiva>
           </meu-painel>
         <!--Quando é atributo eu não posso fazer simplesmente a interpolação, como neste caso, que tenho os atributos da img. Neste caso, eu preciso fazer um v-bind (caso não queira escrever v-bind, posso apenas colocar : antes. -->
       </li>
     </ul>
-  
   </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel.vue';
+import Painel from "./components/shared/painel/Painel.vue";
+import ImagemResponsiva from "./components/shared/imagem-responsiva/ImagemResponsiva.vue";
 
 export default {
-
   components: {
-    'meu-painel' : Painel 
+    "meu-painel": Painel,
+    "imagem-responsiva": ImagemResponsiva
   },
 
   data() {
     return {
       titulo: "Alurapic",
-      fotos: []
+      fotos: [],
+      filtro: ""
     };
+  },
+
+  computed: {
+    fotosComfiltro() {
+      if (this.filtro) {
+        //caso tenha algo para filtrar, eu filtro, senão, eu retorno a lista toda de fotos.
+        let exp = new RegExp(this.filtro.trim(), "i"); //Estou criando uma expressão regular chamada exp baseada no valor digitado no filtro e usando case sensitive.
+        return this.fotos.filter(foto => exp.test(foto.titulo)); //a função filter do JavaScript filtra um lista. Estou retornando uma lista filtrada baseada em uma expressão regular criada anteriormente, que recebe o objeto foto e retorna caso o título dele esteja de acordo com a regra da expressão regular.
+      } else {
+        return this.fotos;
+      }
+
+      // if(this.filtro) {
+      //   let exp = new RegExp(this.filtro.trim(), 'i');
+      //   return this.fotos.filter(function (foto) {
+      //     return exp.test(foto.titulo) //estou testando se o título da foto é igual ao valor digitado pela pessoa com os espaços cortados e em case sentitive. (this.filtro.trim(), 'i')
+      //   });//test retorna true ou false. Ao jogar a legenda da imagem, eu encontrei este padrão definido na expressão regular ou não? É true ou é false?
+      // } else {
+      //   return this.fotos;
+      // }
+    }
   },
 
   created() {
@@ -73,11 +98,12 @@ export default {
   display: inline-block;
 }
 
-.imagem-responsiva {
-  width: 100%;
-}
-
 .lista-fotos .lista-fotos-item {
   display: inline-block;
+}
+
+.filtro {
+  display: block;
+  width: 100%;
 }
 </style>
--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -1,20 +1,30 @@
 <template>
     <div class="painel">
-        <h2 class="painel-titulo">{{ titulo }}</h2>
-        <slot class="painel-conteudo"> <!-- Quando eu coloco o slot, ele entende que tudo que estiver dentro dele, ele vai manter ao invés de trocar. Esta area chamada "slot" vai receber tudo que estiver dentro do "meu-painel"-->
-        </slot>
+        <h2 class="painel-titulo" v-on:dblclick="visivel = !visivel">{{ titulo }}</h2> <!--Quando eu der double click, ele muda o valor da variável visivel. Eu posso escrever v-on ou apenas colocar @. ATENÇÃO, o v-show não pode ser usado diretamente na tag SLOT, por isto eu tive que criar uma div-->
+        <transition name="painel-fade">
+            <div class="painel-conteudo" v-show="visivel"> <!-- Quando eu coloco o slot, ele entende que tudo que estiver dentro dele, ele vai manter ao invés de trocar. Esta area chamada "slot" vai receber tudo que estiver dentro do "meu-painel"-->
+                <!-- A função v-show iniciará com o valor de visivel, pode ser true ou false. Depois eu uso a função v-on:dblclick para disparar um evento quando eu clicar duas vezes.-->
+                <slot></slot>
+            </div>
+        </transition>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['titulo']
+  props: ["titulo"],
+
+  data() {
+    return {
+      visivel: true
+    };
+  }
 };
 </script>
 
 <!-- Quando eu escrevo scoped depois de style, isto significa que este estilo vai valer apenas para este componente que está sendo carregado. Neste caso, o painel. Por exemplo, eu aplico box-shadow universalmente no meu estilo a partir do *, porém ele aplicou este estilo apenas para o componente Painel, uma vez que eu coloquei scoped e restringi este estilo apenas para o componente Painel.-->   
 <!-- É uma boa prática sempre que eu estilizar um componente eu colocar o scoped, para ele aplicar apenas ao elemento. Neste caso, se eu não colocasse escoped, o recarregar meu estilo no APP, este estilo afetará todos os componentes da página, pois ele é universal. Com o scoped, ele é universal apenas para este componente. -->
-<style scoped> 
+<style scoped>
 .painel {
   padding: 0 auto;
   border: solid 2px grey;
@@ -37,6 +47,14 @@ export default {
 }
 
 * {
-    box-shadow: 5px 5px 5px black;
+  box-shadow: 5px 5px 5px black;
 }
+
+.painel-fade-enter, .painel-fade-leave-active {
+    opacity: 0;
+} 
+
+.painel-fade-enter-active, .painel-fade-leave-active {
+    transition: opacity .4s; /*.4s = 400 milisegundos, 4s = 2 segundos*/
+} 
 </style>
